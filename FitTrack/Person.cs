49b5e3b80afb58a63bafd82c54a628199f9540fa
benjaminf-123 <bennefn@hotmail.com>
2024-10-20 12:48:34 +0200

    några klasser tillagda

---
+++ FitTrack/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitTrack
+{
+    public class Person
+    {
+        public string Username { get; set; }
+       public string Password { get; set; }
+
+
+    public virtual void SignIn()
+        {
+
+        }
+
+    }
+}
---
+++ FitTrack/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitTrack
+{
+    public class User : Person
+    {
+        public string Country { get; set; }
+
+        public string SecurityQuestion { get; set; }
+        public string SecurityAnswer { get; set; }
+
+    }
+}
