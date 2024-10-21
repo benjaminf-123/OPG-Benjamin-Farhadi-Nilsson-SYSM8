048813fdad42678754f9dc18d16ff308258d5cb0
benjaminf-123 <bennefn@hotmail.com>
2024-10-21 12:13:34 +0200

    börjat med WorkoutsWindow

---
+++ FitTrack/WorkoutsWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FitTrack
+{
+    /// <summary>
+    /// Interaction logic for WorkoutsWindow.xaml
+    /// </summary>
+    public partial class WorkoutsWindow : Window
+    {
+        public User User { get; set; }
+        public List<Workout> WorkoutList { get; set; }
+
+        public WorkoutsWindow()
+        {
+            InitializeComponent();
+            WorkoutList = new List<Workout>();
+        }
+
+        // Lägga till träningspass
+        private void AddWorkout_Click(object sender, RoutedEventArgs e)
+        {
+            var addWorkoutWindow = new AddWorkoutWindow();
+
+            // Öppna fönstret och lägg till träningspasset
+            if (addWorkoutWindow.ShowDialog() == true)
+            {
+                Workout newWorkout = addWorkoutWindow.NewWorkout;
+                WorkoutList.Add(newWorkout);
+                RefreshWorkoutList();
+            }
+        }
+
+        // Uppdatera listan med träningspass
+        private void RefreshWorkoutList()
+        {
+            WorkoutsList.ItemsSource = null; 
+            WorkoutsList.ItemsSource = WorkoutList; 
+        }
+
+        // Ta bort träningspass
+        private void RemoveWorkout_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedWorkout = (Workout)WorkoutsList.SelectedItem;
+            if (selectedWorkout != null)
+            {
+                WorkoutList.Remove(selectedWorkout);
+                RefreshWorkoutList();
+            }
+        }
+
+        // Visa detaljer om träningspass
+        private void DetailsWorkout_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedWorkout = (Workout)WorkoutsList.SelectedItem;
+            if (selectedWorkout != null)
+            {
+                
+                var detailsWindow = new WorkoutDetailsWindow(selectedWorkout);
+                detailsWindow.ShowDialog();
+            }
+        }
+    }
+}
